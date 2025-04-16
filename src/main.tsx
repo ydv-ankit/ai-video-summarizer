dfff7046f236605f79e2d024465ab1a54711bbab
Ankit Ydv <ankityadav11241@gmail.com>
2025-04-16 21:07:20 +0530

    tailwind css & shadcn setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Button } from "./components/ui/button";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<div>Hello</div>
+		<h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+		<Button>Click me</Button>
 	</StrictMode>
 );
