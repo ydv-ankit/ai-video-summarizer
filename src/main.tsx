e43c8009baa40f86f1ec90f566e4d3c73c368cc4
Ankit Ydv <ankityadav11241@gmail.com>
2025-04-16 21:16:57 +0530

    react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import { Button } from "./components/ui/button";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
-		<Button>Click me</Button>
+		<App />
 	</StrictMode>
 );
