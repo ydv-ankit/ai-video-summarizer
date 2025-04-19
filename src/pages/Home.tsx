a0bb194a7d957c30a4144cd86222116ca5e82d25
Ankit Ydv <ankityadav11241@gmail.com>
2025-04-19 18:37:44 +0530

    responsive ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,25 +3,25 @@ import { Link } from "react-router";
 
 const Home = () => {
 	return (
-		<div className="relative flex items-center justify-center h-screen w-full  bg-gradient-to-br from-indigo-600 via-purple-500 to-pink-500 ">
-			<div className="flex-2/3 flex items-center justify-center text-gray-100">
-				<div className="flex flex-col items-center justify-center mx-24">
-					<div className="md:text-5xl font-bold text-justify">
+		<div className="relative flex  flex-col md:flex-row md:items-center md:justify-center h-screen w-full  bg-gradient-to-br from-indigo-600 via-purple-500 to-pink-500 ">
+			<div className="flex-2/3 flex md:items-center md:justify-center text-gray-100">
+				<div className="flex flex-col items-center md:justify-center mx-4 md:mx-24 mt-10 md:mt-0">
+					<div className="md:text-5xl text-2xl font-bold text-justify">
 						Transform hours of video into concise summaries in minutes.
 					</div>
-					<div className="text-pink-200 mt-6 text-xl">
+					<div className="text-pink-200 mt-6 text-sm md:text-xl">
 						Our AI-powered video summarizer intelligently extracts key moments, ensuring you never
 						miss important content. Experience the future of video consumption with unparalleled
 						efficiency.
 					</div>
 					<div className="flex items-center justify-between mt-8 w-full">
 						<div className="flex flex-col justify-center max-w-52">
-							<div className="text-3xl font-bold">Efficiency</div>
-							<div className="">Save time and enhance your productivity.</div>
+							<div className="text-xl md:text-3xl font-bold">Efficiency</div>
+							<div className="text-sm md:text-lg">Save time and enhance your productivity.</div>
 						</div>
 						<div className="flex flex-col justify-center max-w-52">
-							<div className="text-3xl font-bold">Clarity</div>
-							<div>Get clear insights from lengthy videos.</div>
+							<div className="text-xl md:text-3xl font-bold">Clarity</div>
+							<div className="text-sm md:text-lg">Get clear insights from lengthy videos.</div>
 						</div>
 					</div>
 					<div className="w-full mt-8">
@@ -31,7 +31,7 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
-			<div className="flex-1/3 flex items-center justify-center mx-10">
+			<div className="flex-1/3 hidden lg:flex items-center justify-center mx-10">
 				<img src="icon.ico" alt="logo" className="w-[470px]" />
 			</div>
 		</div>
--- src/pages/Summarizer.tsx
+++ src/pages/Summarizer.tsx
@@ -1,9 +1,11 @@
 import { MarkdownPreview } from "@/components/md-preview";
+import { Button } from "@/components/ui/button";
 import Uploader from "@/components/uploader";
 import useAxios from "@/hooks/useAxios";
 import { env } from "@/lib/env";
 import useAuthStore from "@/store/zustand";
 import { useState } from "react";
+import { Link } from "react-router";
 
 const Summarizer = () => {
 	const { makeAxiosRequest, error, isLoading } = useAxios();
@@ -34,8 +36,11 @@ const Summarizer = () => {
 
 	if (!user) {
 		return (
-			<div className="flex items-center justify-center h-screen w-full bg-gradient-to-br from-indigo-600 via-purple-500 to-pink-500">
-				<div className="text-white text-2xl">Please log in to access this page.</div>
+			<div className="flex flex-col items-center justify-center h-screen w-full bg-gradient-to-br from-indigo-600 via-purple-500 to-pink-500">
+				<div className="text-white text-2xl mx-4">Please log in to access this page.</div>
+				<Link to={"/login"}>
+					<Button variant={"outline"}>Login</Button>
+				</Link>
 			</div>
 		);
 	}
